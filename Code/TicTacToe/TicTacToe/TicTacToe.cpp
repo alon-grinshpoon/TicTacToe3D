11f2257e813559f221a965b2f597cef8975c9562
Alon Grinshpoon <alon.grinshpoon@yahoo.com>
2018-01-31 22:01:43 -0500

    Saving files before refreshing line endings

--- Code/TicTacToe/TicTacToe/TicTacToe.cpp
+++ Code/TicTacToe/TicTacToe/TicTacToe.cpp
@@ -300,7 +300,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
-    return 0;
+    return 0;s
 }
 
 // Message handler for about box.
